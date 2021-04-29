6114fa831655a7f5b5ae3177e7f80520756f4ee3
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2021-04-28 22:41:16 -0700

    Added code to dynamically change info based on dataset selected

---
+++ script.js
@@ -0,0 +1,41 @@
+const datasets = [
+  {
+    name: 'Kickstarter Pledges',
+    description: 'Top 100 Kickstarter Pledges Grouped by Category',
+    group: 'Category',
+    url: 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json'
+  },
+  {
+    name: 'Movie Sales',
+    description: 'Top 100 Highest Grossing Movies Grouped by Genre',
+    group: 'Genre',
+    url: 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json'
+  },
+  {
+    name: 'Video Game Sales',
+    description: 'Top 100 Video Game Sales Grouped by Platform',
+    group: 'Platform',
+    url: 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json'
+  }
+];
+var index = 0;
+
+
+window.onload = () => {
+  updateInfo();
+
+  document.querySelectorAll('.navbar a').forEach(elem => {
+    elem.addEventListener('click', evt => {
+      index = evt.target.getAttribute('data-index');
+      updateInfo();
+    });
+  });
+};  // End window.onload()
+
+
+function updateInfo() {
+  document.querySelectorAll('title, #title').forEach(elm => {
+    elm.innerHTML = datasets[index].name;
+  });
+  document.querySelector('#description').innerHTML = datasets[index].description;
+}  // End updateInfo()
