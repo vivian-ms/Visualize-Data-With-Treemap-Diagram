390ccb5d58ee50d4aae8323848b5efe37c6478cd
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2021-05-01 17:36:44 -0700

    Added function to draw legend

--- script.js
+++ script.js
@@ -19,8 +19,14 @@ const datasets = [
   }
 ];
 var index = 0;
-const w = 800;
-const h = 300;
+const tree = {
+  w: 800,
+  h: 300
+};
+const legend_box = {
+  w: 300,
+  h: 100
+};
 const colors = ['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f'];  // Source: https://colorbrewer2.org/?type=qualitative&scheme=Set3&n=12
 
 
@@ -47,11 +53,11 @@ window.onload = () => {
 
 
 function updateInfo() {
-  document.querySelectorAll('title, #title').forEach(elm => {
-    elm.innerHTML = datasets[index].name;
-  });
+  document.querySelector('title').innerHTML = `Top 100 ${datasets[index].name}`;
+  document.querySelector('#title').innerHTML = datasets[index].name;
   document.querySelector('#description').innerHTML = datasets[index].description;
   document.querySelector('#treemap_container').innerHTML = '';
+  document.querySelector('#legend_container').innerHTML = '';
 }  // End updateInfo()
 
 
@@ -59,7 +65,7 @@ function createCanvas(data) {
   d3.select('#treemap_container')
     .append('svg')
     .attr('id', 'treemap')
-    .attr('viewBox', `0 0 ${w} ${h}`)
+    .attr('viewBox', `0 0 ${tree.w} ${tree.h}`)
     .attr('preserveAspectRatio', 'xMidYMid meet');
 
   drawTreemap(data);
@@ -79,47 +85,47 @@ function drawTreemap(data) {
                      .range(color_array);
 
   let tooltip = d3.select('#treemap_container')
-                 .append('div')
-                 .attr('id', 'tooltip')
-                 .style('opacity', 0);
+                  .append('div')
+                  .attr('id', 'tooltip')
+                  .style('opacity', 0);
 
   let root = d3.hierarchy(data, d => d.children)
                .sum(d => d.value)
                .sort((a, b) => b.value - a.value);
 
   let treemap = d3.treemap()
-                    .size([w, h]);
+                  .size([tree.w, tree.h]);
 
   treemap(root);
 
   let tiles = d3.select('#treemap').selectAll('g')
-               .data(root.leaves())
-               .enter()
-               .append('g')
-               .attr('transform', d => `translate(${d.x0}, ${d.y0})`)
-               .on('mouseover', (evt, d) => {
-                 d3.select(evt.currentTarget.querySelector('rect')).transition()
-                   .duration(50)
-                   .attr('stroke-width', '0.1rem');
-                 tooltip.transition()
-                        .duration(50)
-                        .style('opacity', 1)
-                        .style('left', `${evt.pageX - 150}px`)
-                        .style('top', `${evt.pageY - 300}px`);
-                 tooltip.attr('data-value', d.data.value)
-                        .html(() => {
-                          let format = d3.format(',');
-                          return `<h4>${d.data.name}</h4> <p>${datasets[index].group}: ${d.data.category}</p> <p>Value: ${format(d.data.value)}</p>`;
-                        })
-               })
-               .on('mouseleave', (evt, d) => {
-                 d3.select(evt.currentTarget.querySelector('rect')).transition()
-                   .duration(50)
-                   .attr('stroke-width', '0.01rem');
-                 tooltip.transition()
-                        .duration(50)
-                        .style('opacity', 0);
-               });
+                .data(root.leaves())
+                .enter()
+                .append('g')
+                .attr('transform', d => `translate(${d.x0}, ${d.y0})`)
+                .on('mouseover', (evt, d) => {
+                  d3.select(evt.currentTarget.querySelector('rect')).transition()
+                    .duration(50)
+                    .attr('stroke-width', '0.1rem');
+                  tooltip.transition()
+                         .duration(50)
+                         .style('opacity', 1)
+                         .style('left', `${evt.pageX - 150}px`)
+                         .style('top', `${evt.pageY - 300}px`);
+                  tooltip.attr('data-value', d.data.value)
+                         .html(() => {
+                           let format = d3.format(',');
+                           return `<h4>${d.data.name}</h4> <p>${datasets[index].group}: ${d.data.category}</p> <p>Value: ${format(d.data.value)}</p>`;
+                         })
+                })
+                .on('mouseleave', (evt, d) => {
+                  d3.select(evt.currentTarget.querySelector('rect')).transition()
+                    .duration(50)
+                    .attr('stroke-width', '0.01rem');
+                  tooltip.transition()
+                         .duration(50)
+                         .style('opacity', 0);
+                });
   tiles.append('rect')
        .attr('x', 0)
        .attr('y', 0)
@@ -140,4 +146,33 @@ function drawTreemap(data) {
        .attr('x', 2)
        .attr('y', (d, i) => i * 8 + 8)
        .text(d => d);
+
+  drawLegend(category_array, colorScale);
 }  // End drawTreemap()
+
+
+function drawLegend(data, scale) {
+  let legend = d3.select('#legend_container')
+                 .append('svg')
+                 .attr('id', 'legend')
+                 .attr('viewBox', `0 0 ${legend_box.w} ${legend_box.h}`)
+                 .attr('preserveAspectRatio', 'xMidYMid meet');
+  legend.selectAll('rect')
+        .data(data)
+        .enter()
+        .append('rect')
+        .classed('legend-item', true)
+        .attr('x', (d, i) => (i % 4) * 70 + 15)
+        .attr('y', (d, i) => Math.floor(i / 4) * 15 + 10)
+        .attr('width', 10)
+        .attr('height', 10)
+        .attr('fill', d => scale(d));
+  legend.selectAll('text')
+        .data(data)
+        .enter()
+        .append('text')
+        .classed('legend-text', true)
+        .attr('x', (d, i) => (i % 4) * 70 + 28)
+        .attr('y', (d, i) => Math.floor(i / 4) * 15 + 17)
+        .text(d => d);
+}  // End drawLegend()
